e13fec531aebf2054a8e29da32f820a3eca76903
Marta Pérez Pérez <mperezperez@danielcastelao.org>
2021-11-17 13:23:15 +0100

    Initial commit

---
+++ src/com/Evaluacion1PROG/Main.java
@@ -0,0 +1,9 @@
+package com.Evaluacion1PROG;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hi World!");
+    }
+}
